0003c5081dbf39984488cc0343b565f0da2fbf33
Premkumar734 <prem778071@gmail.com>
2026-01-22 13:38:27 +0530

    Update userdata.sh

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 # User data script for EC2 instances
+
+
+yum update -y
+yum install httpd -y
+systemctl start httpd
+systemctl enable httpd
+echo "Hello from AWS EC2" > /var/www/html/index.html
